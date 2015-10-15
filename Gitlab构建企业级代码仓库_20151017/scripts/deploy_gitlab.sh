904a0bfbe82752b36f383829dbb81835590c2d83
gitlab_test <yj2008ay611@gmail.com>
2015-10-15 15:14:55 +0800

    add gitlab-ce-8.0.5-ce.0.el6.x86_64.rpm

--- Gitlab构建企业级代码仓库_20151017/scripts/deploy_gitlab.sh
+++ Gitlab构建企业级代码仓库_20151017/scripts/deploy_gitlab.sh
@@ -5,13 +5,16 @@
 
 # Install and configure the necessary dependencies
 sudo yum -y install curl openssh-server postfix cronie
-sudo service postfix start
-sudo chkconfig postfix on
-sudo lokkit -s http -s ssh
+#sudo service postfix start
+#sudo chkconfig postfix on
+#sudo lokkit -s http -s ssh
 
 # Add the GitLab package server and install the package
-curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.rpm.sh | sudo bash
-sudo yum -y install gitlab-ce
+#curl https://packages.gitlab.com/install/repositories/gitlab/gitlab-ce/script.rpm.sh | sudo bash
+#sudo yum -y install gitlab-ce
+
+# another way to install gitlab
+rpm -ivh ../rpms/gitlab-ce-8.0.5-ce.0.el6.x86_64.rpm
 
 # Configure and start GitLab
 #sudo gitlab-ctl reconfigure
